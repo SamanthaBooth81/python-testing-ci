f99dfa6534f5cef145d0c97b4860ff1638029840
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-12-07 15:45:37 +0000

    Add extension test and code

--- test_student.py
+++ test_student.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudents(unittest.TestCase):
@@ -7,7 +8,7 @@ class TestStudents(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print('setUpClass')
-    
+
     @classmethod
     def tearDownClass(cls):
         print('tearDowClass')
@@ -32,6 +33,13 @@ class TestStudents(unittest.TestCase):
         self.student.alert_santa()
 
         self.assertTrue(self.student.naughty_list)
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
 
 
 if __name__ == "__main__":
